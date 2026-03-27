6a94de399d7d00dd383b1a1c64c38da8599d51e0
Aukaba <escaperomel@gmail.com>
2026-03-28 00:45:23 +0800

    Merge fix
    merge fix

--- automate/lib/main.dart
+++ automate/lib/main.dart
@@ -20,7 +20,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Automate',
-      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
